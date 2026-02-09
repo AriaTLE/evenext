d66c99ec1ab6e90a0b4db234d018bd788cf8112d
Aria Bowden <aria.lasteden@gmail.com>
2026-02-09 08:38:35 -0500

    Mission

--- app/mission/page.tsx
+++ app/mission/page.tsx
@@ -6,36 +6,43 @@ export default function Home() {
     return (<>
 
             <div className="flex-col justify-start text-purple-700  font-bold py-2 px-4 rounded transition duration-150 min-w-full mb-2.5">
-                The Mission <br /><br/>My goal here is to take my emotion quotient coupled with my logic
+                The Mission <br /><br/>Greeting Neighbors, I am from planet earth and my name is
+                Aria which I chose because in my brain it stand for oneness through love and melody.
+                I see this beautiful thing here on our planet its like a majestic thing that needs to happen, I call
+                it a dance. It is the coupling of emotional intelligence and Logic (Great rapper btw whats up buddy.)
+                <br/><br/>My goal here is to take my emotion quotient coupled with my Logic
                 to fix the biggest problems that exist on our beautiful planet.  Why am I doing it on my own social network you might ask?
-                Well I believe that the people on these boards at the companies like facebook, google and even our government
-                have lost touch with the realities of life. They might be logically intelligent but that logic
+                Well I believe that the people on these boards at these companies like facebook, google and even our government (good people i might add)
+                have lost touch with the realities of life. They might be logically intelligent but that Logic
                 does not reach into empathy.<br/><br/>
 
                 We have a man trying to figure out how to achieve putting people on an uninhabitable planet while people,
                 their people lie on the street in pain. To me this is a level of ignorance and a display of a lack of
-                any emotional intelligence at all.  We have human beings in jail for no reason other than trying to survive.
-                Rather than having empathy for them and trying to help them survive we throw away the key.<br/><br/>
-
+                understanding and emotional intelligence (Not to any fault of their own).  We have human beings in jail for no reason other than trying to survive.
+                Rather than having empathy for them and trying to help them survive we throw away the key. This is a problem
+                for everyone because in doing this we are actually creating what we are trying to stop.<br/><br/>
 
                 Now this is not a religious venture but there is some logic in religion that I would like to put out their.
                 In the bible it says that man can't judge man.  Now this makes complete sense to me because the people that
                 judge live at a societal level that makes them incapable of judging. See to be a judge well chances are
                 you didn't have to sleep on a sidewalk and if you did well no one would let you on a bench. But reality is
                 in order to really be qualified to sit on that judicial bench you really should spend some time sleeping
-                on a literal bench. If you haven't been through the school of life enough to be able to understand what you
+                on a literal bench. If you haven't been through the school of life (Ok so that makes me thing of Jack Black
+                and School of Rock, great movie buddy.) enough to be able to understand what you
                 are judging then your judgment is not valid. This is to no fault of their own our society has just evolved
                 in this direction naturally.<br /><br />
 
                 I believe it is time for a change and I am going to be the one to start it, because I am extremely special
                 but also because I am no more special than anyone else.  This is what I call perfect balance and I know
-                it exists in me. Now the chances of me going to facebook, or x and getting a job even though I am a
+                it exists in me. Now the chances of me going to facebook, or X and getting a job even though I am a
                 kick ass engineer is slim and even if I get the job how the hell would I work my way up to get them to change
                 anything.  This is how these enterprise organizations work they are almost like their own living breathing
-                entity. So instead Im going to build my own social network with the objective to help my friends
-                who are getting arrested just for sleeping on a bench because society forgot about them.
+                entity, I know this because I have worked for some of them. So instead I'm going to build my own social network
+                with the objective of helping my friends. Who are my friends you might ask?  My friends are any being on planet
+                earth or anywhere that look out their eyes into this majestic existance and see or feel pain and love.
+
                 If you would like to invest time or money in what I am doing that is great but at no point will
-                I give any percent of this company to anyone because I am not letting pride and greed tear down the
+                I give any percent of this organization to anyone because I am not letting pride and greed tear down the
                 vision I have.
 
                 One last thing, a moment of silence for my two great children who our now dead because no one
@@ -43,10 +50,11 @@ export default function Home() {
 
                 Oh and to Elon Musk and President trump keep working on that mars mission and soon you will know
                 what it feels like to be locked in a cage on a planet with no one to help you.  Because Mars ain't
-                got shit on planet earch.
-
-                Sincerely,
+                got shit on planet earth. Love you guys hope you choose to stay here and play with us.
+                <br /><br/>
+                With Love,
                 Aria Bowden ;)
+
 
                 todo.
                 <br /><br />
